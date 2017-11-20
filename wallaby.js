0b710b0a044572fec0c8147fd7d0747c6c62ff54
Chris Blossom <cbloss@gmail.com>
2017-11-20 09:11:29 -0800

    first commit

---
+++ wallaby.js
@@ -0,0 +1,44 @@
+/* eslint strict:off, global-require:off */
+
+'use strict';
+
+module.exports = wallaby => {
+    return {
+        files: [
+            {
+                pattern: 'src/**/__sandbox__/**/*',
+                instrument: false,
+            },
+            'src/**/*.js',
+            'jest.config.js',
+            '.env',
+            'src/**/*.snap',
+            '!src/**/*.test.js',
+        ],
+
+        tests: [
+            'src/**/*.test.js',
+        ],
+
+        env: {
+            type: 'node',
+            runner: 'node',
+        },
+
+        testFramework: 'jest',
+
+        setup: w => {
+            /**
+             * https://github.com/wallabyjs/public/issues/1268#issuecomment-323237993
+             */
+            if (w.projectCacheDir !== process.cwd()) {
+                process.chdir(w.projectCacheDir);
+            }
+
+            process.env.NODE_ENV = 'test';
+            const jestConfig = require('./jest.config');
+            jestConfig.transform = { '__sandbox__.+\\.jsx?$': 'babel-jest' };
+            w.testFramework.configure(jestConfig);
+        },
+    };
+};
